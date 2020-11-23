72daeaaa38e3514f3ce2e0c5276e4d17ce61d92f
yohan <yohan.malbranque@gmail.com>
2020-11-23 13:49:01 +0100

    update user controller

--- src/main/java/com/codev/locatrash/controller/UserController.java
+++ src/main/java/com/codev/locatrash/controller/UserController.java
@@ -2,33 +2,26 @@ package com.codev.locatrash.controller;
 
 import com.codev.locatrash.entity.User;
 import com.codev.locatrash.entity.request.AddUserRequest;
-import com.codev.locatrash.repository.UserRepository;
 import com.codev.locatrash.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/user")
 public class UserController {
 
-    private UserRepository ur;
-
-    @Autowired
-    public UserController(UserRepository userRepository) {
-        this.ur = userRepository;
-    }
+    private UserService us;
 
     @GetMapping("/list")
     public List<User> getUsers(){
-        UserService us = new UserService(ur);
         return us.getUsers();
     }
 
     @PostMapping("/add")
     public void addUser(@RequestBody AddUserRequest addUserRequest){
-        UserService us = new UserService(ur);
         us.addUser(addUserRequest);
     }
 }
